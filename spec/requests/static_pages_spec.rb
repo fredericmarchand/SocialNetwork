f0961e8f9d74329a79bfcfbbf14246120a9fc763
fredericmarchand <fred.marchand.23@gmail.com>
2013-09-16 15:12:44 -0400

    Visual changes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -77,7 +77,7 @@ describe "Static pages" do
     click_link "Home"
     click_link "Sign up now!"
     expect(page).to have_title(full_title("Sign up"))
-    click_link "sample app"
+    click_link "social network"
     expect(page).to have_title(full_title(''))
   end
 end
